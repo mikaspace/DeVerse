863e2021ae7421a7d2f0d185ae68aa83857c27bf
OplimoKent <klentoplimo@gmail.com>
2026-04-18 17:48:56 +0600

    Refactor merge_sort function for consistency

--- Sequential_vs_Parallel_Act/sequential_sorting.py
+++ Sequential_vs_Parallel_Act/sequential_sorting.py
@@ -1,19 +1,9 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-    return merge(left, right)
-
 def merge(left, right):
     result = []
     i = j = 0
 
     while i < len(left) and j < len(right):
-        if left[i] < right[j]:
+        if left[i] <= right[j]:
             result.append(left[i])
             i += 1
         else:
@@ -23,3 +13,13 @@ def merge(left, right):
     result.extend(left[i:])
     result.extend(right[j:])
     return result
+
+
+def merge_sort(data):
+    if len(data) <= 1:
+        return data
+
+    mid = len(data) // 2
+    left = merge_sort(data[:mid])
+    right = merge_sort(data[mid:])
+    return merge(left, right)
